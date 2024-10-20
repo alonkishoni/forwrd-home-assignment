c5b88f6d2a5f4c2f0a7df4fbc5196a71eec7f4a8
alonkishoni <alonkishoni@gmail.com>
2024-10-20 22:51:51 +0300

    vite version 5.2.6

---
+++ src/pages/users/UserRow/VirtualizedRowWrapper/VirtualizedRowWrapper.jsx
@@ -0,0 +1,23 @@
+import { memo } from 'react';
+import MemoizedUserRow from '../UserRow.jsx';
+import { areEqual } from 'react-window';
+
+export const VirtualizedRowWrapper = memo(({ data, index, style }) => {
+  const { filteredTempUsers, handleDelete, handleInputChange } = data;
+  const user = filteredTempUsers[index];
+
+  if (!user) {
+    return null; // Render nothing if user is not available
+  }
+
+  return (
+    <MemoizedUserRow
+      key={user.id}
+      index={index}
+      style={style}
+      user={user}
+      handleDelete={handleDelete}
+      handleInputChange={handleInputChange}
+    />
+  );
+}, areEqual);
